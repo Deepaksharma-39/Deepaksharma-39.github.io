a5b8e89ba215ee99919fc8bd6957cbbbb94a9228
deepaksharma-39 <d33paksharma.39@gmail.com>
2022-09-11 22:47:01 +0530

    about section and final template

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        }else{
+            
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    // toggle
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+    })
+})
