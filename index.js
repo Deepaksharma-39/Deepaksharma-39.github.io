d5db9a759e1dfcbf174d34709f1f15d84316fcab
deepaksharma-39 <d33paksharma.39@gmail.com>
2022-09-14 03:37:29 +0530

    final template ready

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ $(document).ready(function(){
             
             $('.navbar').removeClass("sticky");
         }
-    });
-    // toggle
 
+        if(this.scrollY>500){
+            $('.scroll-up-button').addClass("show")
+        }else{
+            $('.scroll-up-button').removeClass("show")
+        }
+    });
+
+    // menu
     $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active")
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     })
+
+
+    // slide up
+
+    $('.scroll-up-button').click(function(){
+        $('html').animate({scrollTop: 0});
+    })
+
+
+    // typing animation
+    let typed=new Typed(".typing",{
+        strings: ["JavaScript","Java", "HTML", "CSS", "Git"],
+        typeSpeed: 100,
+        backSpeed: 90,
+        loop:true
+    })
+
 })
