a98b600f071127f67e6c7ca1381519704bddb1b5
Deepak Sharma <d33paksharma.39@gmail.com>
2024-04-12 23:08:00 +0530

    Add files via upload

---
+++ src/components/popup/projectData.js
@@ -0,0 +1,27 @@
+export const sewakTravels={
+    imgSrc1:"/img/portfolio/sewak/head.JPG",
+    title:"Sewak Travels",
+    subtitle:"Cab Booking Web App",
+    description1:"Sewak Travels is a user-friendly cab booking application designed to streamline the travel experience. With an intuitive interface, users can effortlessly book cabs for various destinations, ensuring reliable transportation at their fingertips, and enjoy convenient payment methods.",
+    description2:"",
+    liveSrc:"https://sewaktravels.com/",
+    githubSrc:"https://github.com/TechPlek-Technologies/SewakTravels"
+}
+export const necc={
+    imgSrc1:"/img/portfolio/necc/head.JPG",
+    title:"NECC Logistics",
+    subtitle:"Logistics website",
+    description1:"NECC Website offers a streamlined logistics platform tailored to meet transportation needs.With intuitive navigation and robust features, NECC Website empowers businesses to optimize their logistics operations seamlessly.",
+    description2:"",
+    liveSrc:"https://ucs.fbp.mybluehostin.me/",
+    githubSrc:"https://github.com/TechPlek-Technologies/NECC"
+}
+export const countryard={
+    imgSrc1:"/img/portfolio/countryard/head.JPG",
+    title:"Countryard",
+    subtitle:"Hotels & resorts",
+    description1:"Countryard Hotel Booking webpage is designed to provide users with the ability to browse through available rooms, view their prices, and explore a gallery of images",
+    description2:"",
+    liveSrc:"https://countryard.com/",
+    githubSrc:"https://github.com/TechPlek-Technologies/Countryard"
+}
