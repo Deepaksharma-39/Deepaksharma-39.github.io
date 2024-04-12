0197fd38c8f03524bd5fba01c1ba6da7de02164b
Deepak Sharma <d33paksharma.39@gmail.com>
2024-04-12 23:05:40 +0530

    Update DetailsPopup.js

--- src/components/popup/DetailsPopup.js
+++ src/components/popup/DetailsPopup.js
@@ -1,21 +1,21 @@
 import Popup from "./Popup";
-const DetailsPopup = ({ open, close }) => {
+const DetailsPopup = ({ open, close,data }) => {
   return (
     <Popup open={open} close={close}>
       <div className="popup_details">
         <div className="top_image">
-          <img src="img/thumbs/4-2.jpg" alt="" />
+          <img src={data.imgSrc1} alt="" />
           <div
             className="main"
-            data-img-url="img/portfolio/4.jpg"
-            style={{ backgroundImage: 'url("img/portfolio/4.jpg")' }}
+            data-img-url={data.imgSrc1}
+            style={{ backgroundImage: `url(${data.imgSrc1})` }}
           />
         </div>
         <div className="portfolio_main_title">
-          <h3>Mockup Phone</h3>
+          <h3>{data.title}</h3>
           <span>
-            <a href="#" onClick={(e) => e.preventDefault()}>
-              Details
+            <a href={data.liveSrc} onClick={(e) => e.preventDefault()}>
+              {data.subtitle}
             </a>
           </span>
           <div />
@@ -23,63 +23,23 @@ const DetailsPopup = ({ open, close }) => {
         <div className="main_details">
           <div className="textbox">
             <p>
-              We live in a world where we need to move quickly and iterate on
-              our ideas as flexibly as possible.
-            </p>
-            <p>
-              Mockups are useful both for the creative phase of the project -
-              for instance when {`you're`} trying to figure out your user flows
-              or the proper visual hierarchy - and the production phase when
-              they phase when they will represent the target product. Building
-              mockups strikes the ideal balance ease of modification.
+              {data.description1}
             </p>
           </div>
-          <div className="detailbox">
+          <div className="detailbox ">
             <ul>
               <li>
-                <span className="first">Client</span>
-                <span>Alvaro Morata</span>
-              </li>
-              <li>
-                <span className="first">Category</span>
                 <span>
-                  <a href="#">Detail</a>
+                  <a style={{ color:" var(--main-color)"}} target="blank" href={data.liveSrc}>Live</a>
                 </span>
               </li>
               <li>
-                <span className="first">Date</span>
-                <span>April 22, 2022</span>
+                <span>
+                  <a style={{ color:" var(--main-color)"}} target="blank" href={data.githubSrc}>Github</a>
+                </span>
               </li>
             </ul>
           </div>
-        </div>
-        <div className="additional_images">
-          <ul>
-            <li>
-              <div className="list_inner">
-                <div className="my_image">
-                  <img src="img/thumbs/4-2.jpg" alt="" />
-                  <div className="main" data-img-url="img/service/1.jpg" />
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="list_inner">
-                <div className="my_image">
-                  <img src="img/thumbs/4-2.jpg" alt="" />
-                  <div className="main" data-img-url="img/service/2.jpg" />
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="list_inner">
-                <div className="my_image">
-                  <img src="img/thumbs/4-2.jpg" alt="" />
-                  <div className="main" data-img-url="img/service/3.jpg" />
-                </div>
-              </div>
-            </li>
-          </ul>
         </div>
       </div>
     </Popup>
